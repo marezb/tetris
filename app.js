e7e12bc00ccb2efc2bdc35c0f22c60d9227c53ed
marezb <marekzbroniec@gmail.com>
2021-04-11 21:09:04 +0200

    controls + next block + play/pause

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-import allBlocks from './blocks.js';
+'use strict';
+
+import { allBlocks, nextBlocks } from './blocks.js';
 
 const grid = document.querySelector('.grid');
+console.log('grid', grid);
+const nextBlockGrid = document.querySelector('.grid-next-block');
+console.log('nextBlockGrid', nextBlockGrid);
 
 //create HTML grids and fill with divs
 for (let i = 0; i < 200; i++) {
@@ -18,19 +23,39 @@ for (let i = 1; i <= 10; i++) {
     grid.appendChild(square);
 }
 
+//create next block grid
+for (let i = 1; i <= 16; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.classList.add('next');
+    nextBlockGrid.appendChild(square);
+}
+
 //////////////////////////////////////////////////////
 
 const gridWidth = 10;
 const randomBlock = () => Math.floor(Math.random() * allBlocks.length);
-let activeBlock = allBlocks[randomBlock()][0];
-let position = 3;
+let choosenBlock = randomBlock();
+console.log('choosenBlock START', choosenBlock);
+let position = 4;
+let rotation = 0;
+let activeBlock = allBlocks[choosenBlock][rotation];
 const playArea = Array.from(document.querySelectorAll('.hidden'));
+console.log('playArea', playArea);
+const nextBlockArea = document.querySelectorAll('.next');
+console.log('nextBlockArea', nextBlockArea);
+let gameSpeed = 11000;
+let nextBlockPosition = 1;
+let nextBlock;
+console.log('nextBlock', nextBlock);
+const playButton = document.querySelector('.play-button');
+console.log('playButton', playButton);
+let timer;
 
 //////////////////////////////////////////////////////
 
 function addClassToDiv(className) {
     activeBlock.forEach(blockSmallSquareLocation => {
-        // console.log(blockSmallSquareLocation);
         playArea[position + blockSmallSquareLocation].classList.add(className);
     });
 }
@@ -39,19 +64,21 @@ function createBlock() {
     addClassToDiv('block');
 }
 
+function resetBlock() {
+    // choosenBlock = randomBlock();
+    position = 4;
+    rotation = 0;
+    choosenBlock = nextBlock;
+    activeBlock = allBlocks[choosenBlock][rotation];
+    console.log('resetBlock | choosenBlock', choosenBlock);
+    createBlock();
+    createNextBlock();
+}
+
 function clearBlock() {
     activeBlock.forEach(blockSquareLocation => {
         playArea[position + blockSquareLocation].classList.remove('block');
     });
-}
-createBlock();
-
-function moveBlockDown() {
-    clearBlock();
-    position += gridWidth;
-    console.log(position);
-    createBlock();
-    stopBlockMovement();
 }
 
 function checkIfBlockIsAtTheBottom(activeBlock) {
@@ -64,40 +91,115 @@ function checkIfBlockIsAtTheBottom(activeBlock) {
 
 function stopBlockMovement() {
     if (checkIfBlockIsAtTheBottom(activeBlock)) {
-        console.log('jestesmy w ifie');
         addClassToDiv('full');
-
-        activeBlock = allBlocks[randomBlock()][0];
-        position = 3;
-        createBlock();
+        resetBlock();
     }
 }
 
-function moveLeft() {
-    console.log('jestesmy w move left');
+function moveBlockDown() {
     clearBlock();
+    position += gridWidth;
+    createBlock();
+    stopBlockMovement();
+}
+
+function controlMovement(e) {
+    switch (e.keyCode) {
+        case 37:
+            return moveLeft();
+        case 38:
+            return rotateBlock();
+        case 39:
+            return moveRight();
+        case 40:
+            return moveBlockDown();
+    }
+}
+
+console.log(11 % gridWidth);
+
+function moveLeft() {
+    clearBlock();
+    console.log(position);
     const isAtTheLeftEdge = activeBlock.some(
         blockSmallSquareLocation =>
-            (blockSmallSquareLocation + position) % gridWidth === 0
+            (position + blockSmallSquareLocation) % gridWidth === 0
     );
-    const isLeftSquareFull = activeBlock.some(blockSquareLocation =>
-        playArea[position - 1 + blockSquareLocation].classList.contains('full')
+
+    activeBlock.forEach(blockSmallSquareLocation => {
+        console.log('moveLeft | position', position);
+        console.log('moveLeft | blockSmallSquareLocation', blockSmallSquareLocation);
+        console.log('modulo =', (position + blockSmallSquareLocation) % gridWidth);
+    });
+
+    console.log('moveLeft | isAtTheLeftEdge', isAtTheLeftEdge);
+    const isLeftSquareFull = activeBlock.some(blockLocation =>
+        playArea[position - 1 + blockLocation].classList.contains('full')
     );
 
     if (!isAtTheLeftEdge && !isLeftSquareFull) position--;
+    if (
+        activeBlock.some(blockSmallSquareLocation =>
+            playArea[position + blockSmallSquareLocation].classList.contains('full')
+        )
+    )
+        position++;
+
+    createBlock();
+    stopBlockMovement();
+}
+
+function moveRight() {
+    clearBlock();
+    console.log(position);
+    const isAtTheRightEdge = activeBlock.some(
+        blockSmallSquareLocation =>
+            (blockSmallSquareLocation + position) % gridWidth === 9
+    );
+    console.log('moveRight | isAtTheRightEdge', isAtTheRightEdge);
+    const isRightSquareFull = activeBlock.some(blockSquareLocation =>
+        playArea[position + 1 + blockSquareLocation].classList.contains('full')
+    );
+    console.log('moveRight | isRightSquareFull', isRightSquareFull);
+
+    if (!isAtTheRightEdge && !isRightSquareFull) position++;
+
+    createBlock();
+    stopBlockMovement();
+}
+
+function rotateBlock() {
+    clearBlock();
+    rotation++;
+    if (rotation === activeBlock.length) rotation = 0;
+
+    activeBlock = allBlocks[choosenBlock][rotation];
 
     createBlock();
 }
 
-function control(e) {
-    switch (e.keyCode) {
-        case 37:
-            console.log('37');
-            moveLeft();
-            break;
-    }
+function createNextBlock() {
+    //first clear next block grid
+    nextBlockArea.forEach(square => square.classList.remove('block'));
+    nextBlock = randomBlock();
+    const showNextBlock = nextBlocks[nextBlock];
+
+    showNextBlock.forEach(nextBlockSmallSquareLocation => {
+        nextBlockArea[nextBlockSmallSquareLocation + nextBlockPosition].classList.add(
+            'block'
+        );
+    });
 }
 
-document.addEventListener('keydown', control);
+document.addEventListener('keydown', controlMovement);
 
-const Timer = setInterval(moveBlockDown, 1400);
+playButton.addEventListener('click', () => {
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+    } else {
+        timer = setInterval(moveBlockDown, gameSpeed);
+        if (!nextBlock) createNextBlock();
+        createBlock();
+    }
+});
--- blocks.js
+++ blocks.js
@@ -1,5 +1,5 @@
 const lShape = [
-    [1, 11, 21, 22],
+    [1, 11, 21, 2],
     [10, 11, 12, 22],
     [1, 11, 21, 20],
     [10, 20, 21, 22],
@@ -34,4 +34,14 @@ const iShape = [
 ];
 const allBlocks = [lShape, zShape, tShape, oShape, iShape];
 
-export default allBlocks;
+const width = 4; //Grid next block width
+//bocks needed to show next block
+const nextBlocks = [
+    [1, width + 1, width * 2 + 1, 2], //l
+    [0, width, width + 1, width * 2 + 1], //z
+    [1, width, width + 1, width + 2], //t
+    [0, 1, width, width + 1], //o
+    [1, width + 1, width * 2 + 1, width * 3 + 1], //i
+];
+
+export { allBlocks, nextBlocks };
